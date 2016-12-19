d328fa562128da0bc4a566f67aa3de59f47d2066
AntonSmyrnoff <a.smyrnoff@gmail.com>
2016-12-19 23:39:03 +0200

    carousel is done

--- js/script.js
+++ js/script.js
@@ -11,6 +11,24 @@ $(function() {
         target: '+=1'
     });
 
+    /*Навигация по карусели*/
+    $('.jcarousel-pagination').jcarouselPagination({
+        item: function(page) {
+            return '<a href="#' + page + '">' + page + '</a>';
+        }
+    });
+
+    $('.jcarousel-pagination').find('a:first-child').addClass('active');//при загрузке страницы присваиваем класс active 1-му элементу
+
+    $('.jcarousel-pagination')
+    .on('jcarouselpagination:active', 'a', function() {
+        $(this).addClass('active');
+    })
+    .on('jcarouselpagination:inactive', 'a', function() {
+        $(this).removeClass('active');
+    });
+
+
 });
 
 
