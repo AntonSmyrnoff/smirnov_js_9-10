f7b5ca0d3535b69b0eeb05d306ff519e5c345683
AntonSmyrnoff <a.smyrnoff@gmail.com>
2016-12-23 17:01:14 +0200

    checkbox_1

--- js/script.js
+++ js/script.js
@@ -36,6 +36,107 @@ $(document).ready(function() {
 });
 
 
+/*-----CHECKBOX-----*/
+
+$(document).ready(function(){
+
+    $(".niceCheck").mousedown(
+    /* при клике на чекбоксе меняем его вид и значение */
+    function() {
+         changeCheck($(this));
+    });
+
+    $(".niceCheck").each(
+    /* при загрузке страницы нужно проверить какое значение имеет чекбокс и в соответствии с ним выставить вид */
+    function() {
+         changeCheckStart($(this));
+    });
+
+});
+
+function changeCheck(el) {
+    /* 
+        функция смены вида и значения чекбокса
+        el - span контейнер дял обычного чекбокса
+        input - чекбокс
+    */
+
+         var el = el,
+              input = el.find("input").eq(0);
+         if(!input.attr("checked")) {
+            el.css("background-position","0 -17px");    
+            input.attr("checked", true)
+        } else {
+            el.css("background-position","0 0");    
+            input.attr("checked", false)
+        }
+         return true;
+}
+
+function changeCheckStart(el) {
+/* 
+    если установлен атрибут checked, меняем вид чекбокса
+*/
+
+    var el = el,
+            input = el.find("input").eq(0);
+          if(input.attr("checked")) {
+            el.css("background-position","0 -17px");    
+            }
+         return true;
+}
+
+
+
+$(document).ready(function() {
+    $(".niceCheck_1").each( /* при загрузке страницы меняем внешний вид чекбокса */
+    
+    function() {
+         changeCheckStart_1($(this));
+    });
+})
+
+function changeCheckStart_1(el) {
+    var $el = el;
+        $input = $el.find('input').eq(0);
+        checkName = $input.attr("name");
+        checkId = $input.attr("id");
+        checkChecked = $input.attr("checked");
+        checkDisabled = $input.attr("disabled");
+        checkTab = $input.attr("tabindex");
+        checkValue = $input.attr("value");
+
+        $el.replaceWith('<span class="niceCheckJs">'+'<input type="checkbox" name="'+checkName+'" id="'+checkId+'" checked="'+checkChecked+'" tabindex="'+checkTab+'" value="'+checkValue+'">'+'</span>');
+
+        if(checkChecked) {
+            $('#'+checkId).closest('span').css("background-position","0 -17px");
+        }
+
+        if(checkDisabled) {
+            $('#'+checkId).closest('span').addClass('niceCheckJsDisabled');
+            $('#'+checkId).attr("disabled","disabled");
+        }
+        /*return true;*/
+
+}
+
+
+
+
+                                            
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
